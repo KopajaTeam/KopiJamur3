1317fae4e78cadfc6167fee819f36b22d566d692
DESKTOP-J5EQLQP\Panji <panjibudi467@gmail.com>
2018-11-06 11:11:13 +0700

    ++

--- application/views/menu/forum.php
+++ application/views/menu/forum.php
@@ -20,7 +20,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <!-- <?php //$this->load->view('user/slide') ?> -->
 	
-<?php $this->load->view('user/headnav') ?>	
+<div id="custom-header">
+	<div class="custom-header-content">
+		<div class="container">
+			<h1 class="page-title">Forum</h1>
+			<div id="breadcrumb">
+				<div  aria-label="Breadcrumbs" class="breadcrumbs breadcrumb-trail">
+					<ul class="trail-items">
+						<li class="trail-item trail-begin"><span><u>Home</u></span></a></li>
+						<li class="trail-item trail-end"><span>Forum</span></li>
+					</ul>
+				</div> <!-- .breadcrumbs -->
+			</div> <!-- #breadcrumb -->
+		</div> <!-- .container -->
+	</div>  <!-- .custom-header-content -->
+</div> <!-- .custom-header -->	
 
 <div id="content" class="site-content default-full-width blog-grid-layout">
 <div class="container">
