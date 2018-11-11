9ac00d0a2b2cf7eeb4d563f4824189f7e86899b1
DESKTOP-FAMSFKF\bahrularif <bahrularif007@gmail.com>
2018-11-11 23:58:23 +0700

    model testimoni

--- application/models/M_testi.php
+++ application/models/M_testi.php
@@ -3,8 +3,10 @@
 *
 */
 	class M_testi extends CI_Model{
-		public function testi($id_testi)
+		public function testi($id_testimoni)
 		{
-			
+			$this->db->join('user','testimoni.id_user = user.id_user',"LEFT");
+			$this->db->join('produk','testimoni.id_produk = produk.id_produk',"LEFT");
+			$this->db->where('id_testimoni',$id_testimoni);
 		}
 	}
