64f8e28e20e0885ce613da00494de8ed5a3fde10
DESKTOP-J5EQLQP\Panji <panjibudi467@gmail.com>
2018-11-17 20:06:50 +0700

    model

--- application/models/Admin_Dashboard.php
+++ application/models/Admin_Dashboard.php
@@ -22,7 +22,7 @@ class Admin_Dashboard extends CI_Model
 	public function update($table,$data,$where){
 		$this->db->update($table,$data,$where);
 	}
-	public function insert($table,$data){
+	public function insert($table,$date){
 		$this->db->insert($table,$data);
 	}
 	function cek_login($table,$where){
