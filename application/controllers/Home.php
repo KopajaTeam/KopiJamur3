e8a288aa8c21ea2a793ea151bd445ba9992f18be
DESKTOP-FAMSFKF\bahrularif <bahrularif007@gmail.com>
2018-11-26 08:50:24 +0700

    fixed view forum dan testi home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,12 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	public function __construct() {
-        parent::__construct();
-    }
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('M_testi');
+		$this->load->model('K_jamur');
+	}
 
 	public function index(){
-		$this->load->view('user/home');
+		$data["forum"] = $this->K_jamur->beforeforum()->result();
+		$data ["testimoni"] = $this->M_testi->testi()->result();
+		$this->load->view('user/home',$data);
 	}
 
 }
