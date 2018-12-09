9dd4c1dc70c13274e94b737e6e688ed3b72fa5a0
DESKTOP-FAMSFKF\bahrularif <bahrularif007@gmail.com>
2018-12-09 14:04:17 +0700

    sutoni ini wiget semua udh tak benerin jadi lebih simple dan ada limit datanya

--- application/models/M_galery.php
+++ application/models/M_galery.php
@@ -9,11 +9,11 @@
 			return $gambar;
 			
 		}
-		// function detgal(){
-		// $this->db->join('kategori_forum','forum.id_kategori_forum = kategori_forum.id_kategori_forum');
-		// $this->db->where('forum.id_user',$id);
-		// return $this->db->get('forum');
-		// }
+		function limitG($id,$table){
+			$this->db->order_by($id,'DESC');
+			$this->db->limit(6);
+			return $this->db->get($table);
+		}
 	}
 
 ?>
--- application/views/dsuser/edit_testimoni.php
+++ application/views/dsuser/edit_testimoni.php
@@ -39,7 +39,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				<!--/col-3-->
 				<div class="col-sm-9" style="" contenteditable="false">
 					<div class="panel panel-default">
-						<div class="panel-heading">Tambah Testimonial</div>
+						<div class="panel-heading">Edit Testimonial</div>
 						<div class="panel-body">
 							<div class="row">
 								<div class="col-md-12 col-sm-6 col-xs-12">
